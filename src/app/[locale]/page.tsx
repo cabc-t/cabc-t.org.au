27249831f8765c959e4f10f9838189caf498df38
Alex Tang <xelat@hotmail.com>
2026-05-27 15:06:29 +1000

    fix: add mandatory key attribute

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -86,6 +86,7 @@ export default async function HomePage({
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {t.pastors.detail.map((detail, index) => (
               <PastorSection 
+                key={index}
                 id={index}
                 pastor={detail}
                 locale={locale}
