365d8132c7efd3ad707131b67061f3054699fbd0
Alex Tang <xelat@hotmail.com>
2026-05-28 11:42:08 +1000

    chore: remove unused redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function RootPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/en");
-  }, [router]);
-
   return null;
 }
