f7a694c0630cd8eaf36d704d40200a019517bf23
Alex Tang <xelat@hotmail.com>
2026-06-23 09:23:29 +1000

    feat: update language switcher to congregation

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
 export const languages = {
   en: { label: "English", nativeLabel: "EN", dir: "ltr" as const },
-  tc: { label: "Cantonese", nativeLabel: "繁", dir: "ltr" as const },
-  sc: { label: "Mandarin", nativeLabel: "简", dir: "ltr" as const },
+  tc: { label: "Cantonese", nativeLabel: "粵語堂", dir: "ltr" as const },
+  sc: { label: "Mandarin", nativeLabel: "国语堂", dir: "ltr" as const },
 } as const;
 
 export type LanguageCode = keyof typeof languages;
